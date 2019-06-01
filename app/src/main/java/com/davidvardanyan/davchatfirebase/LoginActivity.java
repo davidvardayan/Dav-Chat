c7988a07987a96f84d69ca8aae0e6cb9f785809f
David <dav-ss@mail.ru>
2019-06-01 08:26:46 -0700

    Login

--- app/src/main/java/com/davidvardanyan/davchatfirebase/LoginActivity.java
+++ app/src/main/java/com/davidvardanyan/davchatfirebase/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.davidvardanyan.davchatfirebase;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
 
     // UI references.
@@ -37,13 +48,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+           mAuth = FirebaseAuth.getInstance();
 
     }
 
 
     public void signInExistingUser(View v)   {
-
+        attemptLogin();
     }
 
 
@@ -56,15 +67,40 @@ public class LoginActivity extends AppCompatActivity {
 
     private void attemptLogin() {
 
+   String email = mEmailView.getText().toString();
+   String password = mPasswordView.getText().toString();
 
+      if (email.equals("") || password.equals("")) return;
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
 
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("DavChat","signInWithEmail() onComplete " + task.isSuccessful());
 
-
+                if (!task.isSuccessful()) {
+                    Log.d("DavChat","Problem signing in " + task.isSuccessful());
+                    showErrorDialog("There was a problem signing in...");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this,MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                  }
+            }
+        });
 
     }
 
 
+      private void showErrorDialog(String message){
 
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+      }
 
 
 }
